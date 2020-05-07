702455ace3f16f7a5e14c63e968b12a85f76ae93
Krzysztof Błachowicz <krzysztof.blachowicz@gmail.com>
2020-05-07 09:01:51 +0200

    continuous data retrieving based on events

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ var data = {};
 (async () => {
     const browserType = "chromium";
     browser = await playwright[browserType].launch({
-        headless: false,
+        // headless: false,
     });
 
     let context = await browser.newContext();
@@ -21,56 +21,60 @@ var data = {};
         await page.goto(url);
 
         page.on('console', msg => {
-            console.log(msg.text());
+            // console.log(msg.text());
+            getDepartures(page);
         });
 
         await page.addScriptTag({
             path: 'observer.js'
         });
-
-        let stopoverNameElem = await page.$('.block-search__stop td strong');
-        let stopoverName = await stopoverNameElem.innerText();
-
-        let stopoverNoElem = await page.$('.block-search__stop td span');
-        let stopoverNo = await stopoverNoElem.innerText();
-
-        let departuresRows = await page.$$('table.stop-departures tr');
-
-        // No departures?
-        if (departuresRows.length == 0) {
-            data[`${stopoverName} ${stopoverNo}`] = [];
-            await page.close();
-            processData();
-        }
-        let departuresArray = [];
-        departuresRows.forEach(async (d) => {
-            let departure = {};
-            let lineNoElem = await d.$('.line-no');
-            departure.lineNumber = await lineNoElem.innerText();
-
-            let lineNameElem = await d.$('.line-direction');
-            departure.lineName = await lineNameElem.innerText();
-
-            let lineTimeElem = await d.$('.departure-time');
-            departure.lineTime = await lineTimeElem.innerText();
-
-            departuresArray.push(departure);
-            if (departuresArray.length == departuresRows.length) {
-                data[`${stopoverName} ${stopoverNo}`] = departuresArray;
-                // await page.close();
-                processData();
-            }
-        });
     });
 
 })();
 
+async function getDepartures(page) {
+    let stopoverNameElem = await page.$('.block-search__stop td strong');
+    let stopoverName = await stopoverNameElem.innerText();
+
+    let stopoverNoElem = await page.$('.block-search__stop td span');
+    let stopoverNo = await stopoverNoElem.innerText();
+
+    let departuresRows = await page.$$('table.stop-departures tr');
+
+    // No departures?
+    if (departuresRows.length == 0) {
+        data[`${stopoverName} ${stopoverNo}`] = [];
+        await page.close();
+        processData();
+    }
+    let departuresArray = [];
+    departuresRows.forEach(async (d) => {
+        let departure = {};
+        let lineNoElem = await d.$('.line-no');
+        departure.lineNumber = await lineNoElem.innerText();
+
+        let lineNameElem = await d.$('.line-direction');
+        departure.lineName = await lineNameElem.innerText();
+
+        let lineTimeElem = await d.$('.departure-time');
+        departure.lineTime = await lineTimeElem.innerText();
+
+        departuresArray.push(departure);
+        if (departuresArray.length == departuresRows.length) {
+            data[`${stopoverName} ${stopoverNo}`] = departuresArray;
+            // await page.close();
+            processData();
+        }
+    });
+}
+
 async function processData() {
+    // Not all data retrieved yet
     if (Object.keys(data).length < urls.length) {
         return;
     }
     console.log(data);
-    console.info("Closing browser...");
+    data = [];
     // await browser.close();
     // process.exit();
 }
